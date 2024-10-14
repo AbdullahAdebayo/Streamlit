496bb9830b610b884c8e9fc6b2fbfe2e8998dca7
AbdullahAdebayo <abdullahiadebayo71@gmail.com>
2024-10-14 01:26:54 +0100

    Update

--- first_test.py
+++ first_test.py
@@ -10,3 +10,19 @@ tickerDf = tickerData.history(period = '3mo' , start = '2017-08-02' , end = '201
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
+
+
+#Task2
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+st.title("Stock Price Viewer")
+
+tickerSymbol = "MSFT"
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period = "5y" , interval = '1mo' , start = '1990-02-01' , end = '1995-02-01')
+
+st.line_chart(tickerDf.Close)
